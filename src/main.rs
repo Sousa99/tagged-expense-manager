2452e657bb158f0a68f706165b10605dafb53ed4
Rodrigo Sousa <rod_sousa@hotmail.com>
2024-07-27 22:49:14 +0100

    feat: propper setup with environment variables

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-use log;
+use dotenvy::dotenv;
 
 fn main() {
+    // Load Environment Variables
+    dotenv().expect(".env file could not found");
+
     // Initialize Logger
     env_logger::init();
 
