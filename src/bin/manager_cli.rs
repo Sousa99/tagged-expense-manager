05e328da860ef967ecb444bd483e984681cc56ef
Rodrigo Sousa <rod_sousa@hotmail.com>
2024-07-28 11:50:45 +0100

    chore: standardization of project structure

---
+++ src/bin/manager_cli.rs
@@ -0,0 +1,13 @@
+use dotenvy::dotenv;
+use tagged_expense_manager::database;
+
+fn main() {
+    // Load Environment Variables
+    dotenv().expect("🚫 .env file could not found");
+
+    // Initialize Logger
+    env_logger::init();
+
+    log::info!("Starting up main application 🚀");
+    let _database_connection = database::connection::establish_connection();
+}
--- src/database/connection.rs
+++ src/database/connection.rs
@@ -1,32 +1,20 @@
 use std::env;
 use std::process;
 
-use dotenvy::dotenv;
 use diesel::prelude::*;
 
-pub mod schema;
-pub mod models;
-
-fn main() {
-    // Load Environment Variables
-    dotenv().expect("🚫 .env file could not found");
-
-    // Initialize Logger
-    env_logger::init();
-
-    log::info!("Starting up main application 🚀");
-
-    // Establishing Connection with Database
+pub fn establish_connection() -> SqliteConnection {
     let database_url = env::var("DATABASE_URL").unwrap_or_else(|_| {
         log::error!("Environment variable 'DATABASE_URL' could not be loaded");
         process::exit(1);
     });
 
     log::debug!("Attempting to establish connection with: {}", database_url);
-    let _database_connection = SqliteConnection::establish(&database_url).unwrap_or_else(|_| {
+    let database_connection = SqliteConnection::establish(&database_url).unwrap_or_else(|_| {
         log::error!("Connection could not be established with database");
         process::exit(1);
     });
 
     log::info!("Connection established with database");
+    database_connection
 }
